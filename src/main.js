29a181d9c85a21500039ac0e44b03bc6169b5ec7
sochima <kodejuice@gmail.com>
2025-03-18 17:49:39 +0100

    Adjust maxTrades parameter in placeTrades function for swing trades to improve execution timing

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ async function main() {
   console.log("\n[Begin placing trade]");
 
   await placeTrades("scalp");
-  await placeTrades("swing", 3);
+  await placeTrades("swing", 1);
 
   console.log("\nWaiting for 7 minutes...\n");
   await waitFor(60 * 7);
@@ -31,6 +31,8 @@ async function placeTrades(tradeType, maxTrades = null) {
     await metaTradeAPI.openTrade(params);
     trades++;
     if (maxTrades && trades >= maxTrades) break;
+
+    await waitFor(15);
   }
 }
 
